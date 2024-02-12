180ae19dbc8ab9df4d3469c1b0a2027ca92a0366
Kamil Kobylarz <xraxus@gmail.com>
2024-02-12 11:53:41 +0100

    Tweak paths to relative

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function Dashboard() {
   return (
   <>
-  <h1>This is dashboard</h1>;
+  <h1>This is dashboard</h1>
   <Outlet/>
   </>)
 }
