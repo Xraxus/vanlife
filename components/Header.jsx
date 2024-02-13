7d37a0bd614085f3bd0ad89663b595b3f7057b96
Kamil Kobylarz <xraxus@gmail.com>
2024-02-13 16:41:39 +0100

    Fix profile img

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import imageUrl from "/assets/images/avatar-icon.png";
 
 export default function Header() {
   const activeStyles = {
@@ -15,25 +16,25 @@ export default function Header() {
       </Link>
       <nav>
         <NavLink
-          to="/host"
+          to="host"
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Host
         </NavLink>
         <NavLink
-          to="/about"
+          to="about"
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           About
         </NavLink>
         <NavLink
-          to="/vans"
+          to="vans"
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Vans
         </NavLink>
         <Link to="login" className="login-link">
-          <img src="../assets/images/avatar-icon.png" className="login-icon" />
+          <img src="/assets/images/avatar-icon.png" className="login-icon" />
         </Link>
       </nav>
     </header>
